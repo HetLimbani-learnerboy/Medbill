c9150138a35ce6ab11e9e16bf09775949efa44a7
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2026-03-08 10:57:32 +0530

    edit

--- Backend-flask/app/models/user_model.py
+++ Backend-flask/app/models/user_model.py
@@ -27,5 +27,8 @@ class User(db.Model):
     def check_password(self, password: str) -> bool:
         return check_password_hash(self.password_hash, password)
 
+
+
     def __repr__(self):
         return f"<User {self.username}>"
+    
