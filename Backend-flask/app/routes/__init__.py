94a0c521e08b8c3e0fdf7723177c5bd94c308f3a
HetLimbani-learnerboy <hetlimbai61@gmail.com>
2026-03-22 15:57:18 +0530

    delete file

--- Backend-flask/app/routes/__init__.py
+++ Backend-flask/app/routes/__init__.py
@@ -1,11 +1,8 @@
 from .auth_routes import auth_bp
 from .receipt_routes import receipt_bp
 from .medicine_routes import medicine_bp
-from app.routes.barcode_routes import barcode_bp
-
     
 def register_blueprints(app):
     app.register_blueprint(auth_bp)
     app.register_blueprint(receipt_bp)
     app.register_blueprint(medicine_bp)
-    app.register_blueprint(barcode_bp)
