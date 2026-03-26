73e91bd5bd2e0db45a549a783d88b1cc4c2ce9c4
S-D123 <shrimandasadiya125@gmail.com>
2026-03-26 15:41:02 +0530

    Fixing Deploy

--- Backend-flask/run.py
+++ Backend-flask/run.py
@@ -7,12 +7,12 @@ app = create_app()
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
 def serve_react_app(path):
-    # If the path is a real file inside web-build, serve the file
-    if path != "" and os.path.exists(os.path.join(app.static_folder, path)):
-        return send_from_directory(app.static_folder, path)
-    
-    # Otherwise, hand it over to React Router by serving index.html
-    return send_from_directory(app.static_folder, 'index.html')
+    static_folder = os.path.join(os.getcwd(), 'dist')
+
+    if path != "" and os.path.exists(os.path.join(static_folder, path)):
+        return send_from_directory(static_folder, path)
+
+    return send_from_directory(static_folder, 'index.html')
 
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
